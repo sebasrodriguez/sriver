7ad28d7c074cba07270dd8c70decdab2b5772985
smaidana83@gmail.com <smaidana83@gmail.com@9610084d-2c2c-ce9e-6b71-ebe9957593fb>
2012-02-18 15:58:35 +0000

    El Facade tiene errores porque solo estan los metodos sin su desarrollo y comentado

--- trunk/SilverRiverCore/src/logica/Facade.java
+++ trunk/SilverRiverCore/src/logica/Facade.java
@@ -11,7 +11,16 @@ public class Facade {
 	private ArrayList<Board> activeGames;
 	
 	
-	//NO HAGO EL CONSTRUCTOR HASTA DEFINIR SI QUEDA COMO SINGLETON
+	/*
+	 * Constructores
+	 */
+	Facade(){
+		
+	}
+	
+	Facade(ArrayList<Board> activeGames){
+		this.activeGames = activeGames;
+	}
 	
 	/*
 	 * Getters
@@ -27,5 +36,88 @@ public class Facade {
 		this.activeGames = activeGames;
 	}
 	
-	//FALTAAAAAA
+	/*
+	 * Acciones
+	 */
+	
+	/*
+	 * Entrada: Id de la partida, barco que se mueve, destino
+	 * Salida: MoveAction
+	 * Procedimiento: 
+	 * Al barco que se mueve le actualiza el destino en la partida
+	 * Coloca en la Queue del otro jugador esta accion
+	 */
+	public MoveAction move(int gameId, Ship shipMoving, Coordenate destination){
+		
+	}
+	
+	/*
+	 * Entrada: Id de la partida, barco que dispara, punto la que dispara
+	 * Salida: FireAction
+	 * Procedimiento:
+	 * Calcula el punto exacto del disparo
+	 * Verifica que haya o no acertado
+	 * Si acerto
+	 * 	actualiza los valores del barco dañado
+	 * actualiza los valores del barco que disparo
+	 * devuelve el fireAction	 	
+	 */
+	public FireAction fire(int gameId, Ship sihpFiring, Coordenate firingPoint){
+		
+	}
+	
+	/*
+	 * Entrada: Id de la partidam, barco que rota y cardianl de destino
+	 * Salida: RotateAction
+	 * Procedimiento:
+	 * Actualiza la posicion a la cual apunta el barco
+	 * devuelve el rotateAction 
+	 */
+	public RotateAction rotate(int gameId, ship shipRotating, Cardinal destination){
+		
+	}
+	
+	/*
+	 * Entrada: Id de la partida
+	 * Salida: EndTurnAction
+	 * Procedimiento: 
+	 * Cambia al jugador activo
+	 * carga la cantidad de movimientos disponibles 
+	 * devuelve endTurnAction
+	 */
+	public EndTurnAction endTurn(int gameId){
+		
+	}
+	
+	/*
+	 * Entrada: Id de la partida
+	 * Salida: void
+	 * Procedimiento:
+	 * Llama al metodo saveGame en la clase Board 
+	 */
+	public void saveGame(int gameId){
+		
+	}
+	
+	/*
+	 * TENGO DUDAS CON ESTE METODO DE COMO DEBERIA SER
+	 * NO DEBERIA RECIBIR LA COMBINACION DE JUGADORES PARA SABER REALMENTE CUAL RECUPERAR?
+	 */
+	public void loadGame(int gameId){
+		
+	}
+	
+	/*
+	 * QUE HARIA EL ENDGAME??????
+	 */
+	public void endGame(int gameId){
+		
+	}
+	
+	/*
+	 * NO ENTIENDO EL PORQUE DE ESTE METODO
+	 */
+	public ArrayList<Action> listAction(int gameId){
+		
+	}
 }
